fb2d4f7781da90051e1266f000744e6297e97f50
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-08-16 07:45:22 +0200

    reproduced

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:some.db")) {
+			System.out.println(conn);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
